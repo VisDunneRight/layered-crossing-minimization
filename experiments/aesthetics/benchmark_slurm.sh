9a479d82de7f46c9c3b94476614d4ceeaef06a5f
cwilson22 <wilson.conn@northeastern.edu>
2025-02-20 10:25:47 -0500

    More results V02

--- experiments/aesthetics/benchmark_slurm.sh
+++ experiments/aesthetics/benchmark_slurm.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=short        # Use short partition (24hrs max)
 #SBATCH -N 1                   # Number of nodes
 #SBATCH -n 1                  # Number of tasks
-#SBATCH --time 12:00:00           # Request 12 hours of compute time
+#SBATCH --time 18:00:00           # Request 12 hours of compute time
 #SBATCH --mem=8G           # Request 8GB memory
 #SBATCH --constraint=broadwell           # Run job on the broadwell hardware cluster
 #SBATCH -o outputs/aesthetics_%A_%a.txt       # Standard output file
