89e5833cfbb75c77e713018b531f52951e36e5eb
cwilson22 <csw8@buffalo.edu>
2025-04-04 14:38:39 -0400

    Bug fix aesthetics_exp

--- aesthetics_hybrid_exp.py
+++ aesthetics_hybrid_exp.py
@@ -159,7 +159,7 @@ if __name__ == '__main__':
     conditions = ['combo_idx']
     csv_header = ['Index', 'Data'] + conditions + ['Nodes', 'Edges', 'TotalNodes', 'TotalEdges', 'ObjVal', 'Crossings', 'EdgeLength', 'Runtime', 'Status']
     condition_map = {
-        'combo_idx': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]}
+        'combo_idx': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]}
     cr_map = get_crossing_optvals(files)
 
     pid = int(sys.argv[1])
